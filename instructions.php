115790d669d98658edbbaa6b5e0f2c78c162a350
David Tidd <tidd.david@gmail.com>
2017-02-01 13:40:46 -0500

    Explorer Instructions

--- instructions.php
+++ instructions.php
@@ -1,5 +1,47 @@
-<h3 style="text-align: center">How to Use the Explorer:</h3>
-<p>The <b>interactive data explorer</b> allows you to generate custom graphs and data tables on the questions and demographics that you find most interesting.</p>
-<h4>1. Click on a category to see a list of questions.</h4>
-<p>In the blue bar to the left, all questions have been grouped into categories. Click one to expand it.</p>
+<div style="max-width:1000px; margin: 0 auto">
+    <h3 style="text-align: center">How to Use the Explorer:</h3>
+    <h4>1. Select the question you want to examine</h4>
+    <div class="row">
+        <div class="col-md-6">
+            <ul><li>In the first row, click the 'Select a question' drop-down to view all questions. You can type in this box to search through the list.
+                    For example, typing 'marijuana' will show only questions containing that word.</li></ul>
+        </div>
+        <div class="col-md-6">
+            <img src="img/help-search-sm.PNG" style="display: block; margin: 0 0 10px;">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <ul><li>You can also filter the questions list by selecting a category in the 'All Categories' drop-down.
+                    Select 'Bullying', for example, to reduce the questions list to only bullying-related items.</li></ul>
+        </div>
+        <div class="col-md-6">
+            <img src="img/help-category-sm.PNG" style="display: block; margin: 0 0 10px;">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <ul><li>After selecting a question, click 'Generate Graph' to create your custom graph and data table.</li></ul>
+        </div>
+    </div>
+    <h4>2. Additional Options</h4>
+    <div class="row">
+        <div class="col-md-6">
+            <ul><li>You optionally can select a second question in the second row. This will compare the first question to the second one.
+                    For instance, you can explore how cigarette use varies with alcohol use.</li></ul>
+        </div>
+        <div class="col-md-6">
+            <img src="img/help-twoquestions-sm.PNG" style="display: block; margin: 0 0 10px;">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <ul><li>You can filter the data by selecting a year, grade, gender, or race in the last row. This will show only data for the selected group.
+                    You could view stress level for only 12th graders.</li></ul>
+        </div>
+        <div class="col-md-6">
+            <img src="img/help-filter-sm.PNG" style="display: block; margin: 0 0 10px;">
+        </div>
+    </div>
+</div>
 
